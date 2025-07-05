1c795b1814768b483152eb269b18815dd57a297e
Josephmusngi21 <josephmusngi7@gmail.com>
2025-07-05 16:57:09 -0700

    initiated Login and Register pages

---
+++ client/components/pages/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import {View, Text, TextInput, StyleSheet} from "react-native";
+
+export default function Login() {
+    
+
+    return(
+        <View style={styles.container}>
+            <Text>Login Page</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+    },
+});
---
+++ client/components/pages/Register/Register.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import {View, Text, TextInput, StyleSheet} from "react-native";
+
+export default function Register() {
+    
+
+    return(
+        <View style={styles.container}>
+            <Text>Register Page</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+    },
+});
