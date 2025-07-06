193bf9ac2b962c001190c93415452d2a6a25018b
Josephmusngi21 <josephmusngi7@gmail.com>
2025-07-05 17:03:54 -0700

    added form for input and submission

--- client/components/pages/Login/Login.jsx
+++ client/components/pages/Login/Login.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
-import {View, Text, TextInput, StyleSheet} from "react-native";
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 
 export default function Login() {
-    
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return(
-        <View style={styles.container}>
-            <Text>Login Page</Text>
+  const handlePress = () => {
+    // Handle login logic here
+    console.log("Login pressed with email:", email, "and password:", password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text>Login Page</Text>
+        <Text>Hey, Enter your details to get sign in to your account</Text>
+      </View>
+      <View style={styles.form}>
+        <View style={styles.input}>
+          <TextInput placeholder="Enter Email/Phone No." />
+          <TextInput placeholder="Enter Password" secureTextEntry={true} />
         </View>
-    )
+        <Text>Having trouble in sign in?</Text>
+        <Button onPress={handlePress()} />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-    },
+  container: {},
 });
